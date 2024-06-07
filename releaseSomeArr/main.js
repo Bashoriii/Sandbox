1ca683739780a8368af3eebbf448a63299d00a3b
Max <shoothead84@gmail.com>
2024-06-07 08:18:19 +0700

    Final look

--- releaseSomeArr/main.js
+++ releaseSomeArr/main.js
@@ -1,29 +1,66 @@
 // Generate random value using math.random() with value range between 0 - 100
-const generateRandomArr = () => {
-  const someArr = [];
+const getArr = (...arrayInputParam) => {
+  const sumUpAllArray = arrayInputParam.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
 
-  for (let i = 0; i < 30; i++) {
-    const randomFloatNum = Math.random() * (100 - 1) + 1;
-    someArr.push(randomFloatNum.toFixed(2));
-  }
-
-  const getResultValue = someArr.map((item) => {
-    return item;
-  });
-
-  return getResultValue;
+  return { sumUpAllArray, arrayInputParam };
 };
 
 // Display it to the DOM
 const displayArr = () => {
-  const containerElem = document.querySelector('#arrContainer');
-  const getRandomValue = generateRandomArr();
-  containerElem.textContent = getRandomValue.join(', ');
+  const inputArrElem = document.querySelector('#inputArr');
+  const totalValueElem = document.querySelector('#totalValue');
+  const releaseTargetElem = document.querySelector('#releaseTarget');
+  const releaseNumElem = document.querySelector('#subsetsId');
+  const totalValueSumElem = document.querySelector('#totalSum');
+
+  const inputArrayValue = inputArrElem.value.split(', ').map(Number);
+  const inputReleaseNumber = parseInt(releaseTargetElem.value);
+
+  const resultArr = getArr(...inputArrayValue);
+  const resultRelease = closestSubset(inputReleaseNumber, inputArrayValue);
+
+  totalValueElem.textContent = resultArr.sumUpAllArray;
+  releaseNumElem.textContent = resultRelease.closestSubset;
+  totalValueSumElem.textContent = resultRelease.closestSum;
+};
+
+const closestSubset = (releaseTargetParam, myArr) => {
+  const n = myArr.length;
+  let closestSum = Number.MAX_VALUE;
+  let closestSubset = [];
+
+  function findSubsets(subset, index) {
+    if (index === n) {
+      const sum = subset.reduce(
+        (accumulator, currentVal) => accumulator + currentVal,
+        0
+      );
+
+      if (
+        Math.abs(releaseTargetParam - sum) <
+        Math.abs(releaseTargetParam - closestSum)
+      ) {
+        closestSum = sum;
+        closestSubset = subset.slice();
+      }
+
+      return 201;
+    }
+
+    subset.push(myArr[index]);
+    findSubsets(subset, index + 1);
+
+    subset.pop();
+    findSubsets(subset, index + 1);
+  }
+
+  findSubsets([], 0);
+
+  return { closestSubset, closestSum };
 };
 
 // Trigger button for display an array
-document.querySelector('#populateArr').addEventListener('click', displayArr);
-
-// Note:
-// Buat jumlahin semua yang ada di dalem array, pake reduce()
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+document.querySelector('#getValueArr').addEventListener('click', displayArr);
